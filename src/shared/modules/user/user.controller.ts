02c8a101749b7f77c17ad71aa5dd394732c0487b
Ilya Kolmakov <ilkolmakov@yandex.ru>
2024-11-01 23:59:26 +0500

    module7-task1: fix several bugs, delete comments

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -83,7 +83,7 @@ export class UserController extends BaseController {
 
   public async showUserFavorites(_: Request, res: Response) {
     // TODO: Токен берется из токена авторизации
-    const userId = "67152f430ace5d6726f44745"
+    const userId = '67152f430ace5d6726f44745';
 
     const existsUser = await this.userService.findById(String(userId));
 
