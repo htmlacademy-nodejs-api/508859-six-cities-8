0b4233f8c3170e4ab4d39c420df841cd7c67cd2b
Ilya Kolmakov <ilkolmakov@yandex.ru>
2024-11-06 14:58:42 +0500

    master: add cli command in scripts and add middleware in checkAuth

--- src/shared/libs/rest/middleware/validate-objectid.middleware.ts
+++ src/shared/libs/rest/middleware/validate-objectid.middleware.ts
@@ -8,7 +8,7 @@ import { HttpError } from '../errors/index.js';
 export class ValidateObjectIdMiddleware implements Middleware {
   constructor(private param: string) {}
 
-  public execute({ params }: Request, _res: Response, next: NextFunction): void {
+  public execute({ params }: Request, _: Response, next: NextFunction): void {
     const objectId = params[this.param];
 
     if (Types.ObjectId.isValid(String(objectId))) {
